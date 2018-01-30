13d779e5b54395ad332fcb87447499eb589671e2
Pazhyn Serhii <pazhinsa@gmail.com>
2018-01-30 10:41:45 +0200

    HW7-8 960 finished

--- HW7-8/gulpfile.js
+++ HW7-8/gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require ('gulp');
 var sass = require ('gulp-sass');
 
 gulp.task ('sass', function(){
-    gulp.src('./css/**/*.scss')
+    gulp.src('./**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task ('sass:watch', function(){
-    gulp.watch('./css/**/*.scss', ['sass']);
+    gulp.watch('./**/*.scss', ['sass']);
 });
